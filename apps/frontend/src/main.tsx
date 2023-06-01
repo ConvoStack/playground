8aa16b0a7e5e4a7cefe924f5a2a66e25d35982fd
radami <imanwiz1@gmail.com>
2023-06-01 17:05:20 -0400

    updated convostack version

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,21 +1,22 @@
 // @ts-ignore
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ConvoStackWrapper } from "convostack/frontend-react";
+import { ConvoStackWrapper, ConvoStackWidget } from "convostack/frontend-react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
-    <ConvoStackWrapper
-      graphqlUrl={import.meta.env.VITE_GRAPHQL_URL}
-      websocketUrl={import.meta.env.VITE_WS_URL}
-      customStyling={{
-        headerText: "Hello, ConvoStack",
-        headerTextColor: "white",
-        iconsColor: "white",
-        widgetLocation: "left",
-      }}
-    >
+    <ConvoStackWrapper>
+      <ConvoStackWidget
+        graphqlUrl={import.meta.env.VITE_GRAPHQL_URL}
+        websocketUrl={import.meta.env.VITE_WS_URL}
+        customStyling={{
+          headerText: "Hello, ConvoStack",
+          headerTextColor: "white",
+          iconsColor: "white",
+          widgetLocation: "left",
+        }}
+      />
       <App />
     </ConvoStackWrapper>
   </>
